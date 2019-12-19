a1d185220b94ce38844d4de83ac84ef8b9e6e793
chikeorjih <chikeorjih@gmail.com>
2019-12-18 21:39:18 -0600

    Updates

---
+++ precache-manifest.c1b011382f42c1297b10647f651803ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a115d42e559e2e70bbabe8794b4b191",
+    "url": "/statjunkie/index.html"
+  },
+  {
+    "revision": "aa078538beb8d5b0b41f",
+    "url": "/statjunkie/static/css/main.7cc54ecc.chunk.css"
+  },
+  {
+    "revision": "9452f3ec08a3d5593b82",
+    "url": "/statjunkie/static/js/2.9be4c1b8.chunk.js"
+  },
+  {
+    "revision": "aa078538beb8d5b0b41f",
+    "url": "/statjunkie/static/js/main.29911039.chunk.js"
+  },
+  {
+    "revision": "a153408e07e1da731ab5",
+    "url": "/statjunkie/static/js/runtime~main.4bd2d2d2.js"
+  }
+]);
